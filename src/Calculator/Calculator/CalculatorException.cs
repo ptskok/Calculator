aed739b9f390bab307a1c0e5c45e03cbe593e99a
Peter Skok - VSC <peter.skok@student.tuke.sk>
2022-05-16 21:13:50 +0200

    implemented CalculatorException functionality

--- src/Calculator/Calculator/CalculatorException.cs
+++ src/Calculator/Calculator/CalculatorException.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    public class CalculatorException
+    public class CalculatorException :Exception
     {
+        public CalculatorException()
+        {
+        }
+        public CalculatorException(string message)
+        :base(message)
+        {
+        }
+        public CalculatorException(string message, Exception inner)
+        :base(message, inner)
+        {
+        }
     }
 }
